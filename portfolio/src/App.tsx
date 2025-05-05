5c2eb2becbea9e8830a9941466f1218e653a06de
monicalu <monicalu1128@hotmail.com>
2025-05-05 13:41:47 -0400

    Initial Setup
    -Setup for Vite, showing white page with hello world

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,15 @@
+// import React, { Suspense, lazy } from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// const Home = lazy(() => import('./pages/Home'));
+// const About = lazy(() => import('./pages/About'));
+// const ComingSoon = lazy(() => import('./pages/ComingSoon'));
+// const NotFound = lazy(() => import('./pages/NotFound'));
+
+function App() {
+    return (
+        <> Hello World!</>
+    )
+}
+
+export default App;
---
+++ portfolio/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+    base: '/Portfolio-Website/',
+    build: {outDir: 'build'},
+    plugins:[
+        react(),
+        viteTsconfigPaths()
+    ],
+    server: {
+        port: 3000,
+        host: true,
+        strictPort: true,
+        hmr: {
+            port: 3010,
+        },
+        watch:{
+            usePolling: true,
+        }
+    }
+})
