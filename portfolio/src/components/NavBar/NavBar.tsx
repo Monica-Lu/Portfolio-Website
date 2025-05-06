c32da380461e70cbc11c5e07ac6a873565a1b497
monicalu <monicalu1128@hotmail.com>
2025-05-06 15:55:06 -0400

    Eslint Error Checking

--- portfolio/src/components/NavBar/NavBar.tsx
+++ portfolio/src/components/NavBar/NavBar.tsx
@@ -1,21 +1,21 @@
-import react, {useEffect, Suspense} from 'react';
-import {useResetRecoilState} from 'recoil';
 import * as NavMenu from "@radix-ui/react-navigation-menu";
 
 import './NavBar.css';
 
 export const NavBar = () => {
     return (
-        <NavMenu.Root className="NavBar">
+        <NavMenu.Root className="NavBar" delayDuration={1000}>
+
+            <NavMenu.Link className='logo-link' href="/">
+                <div className='logo-text'>Monica Lu</div>
+            </NavMenu.Link>
+
             <NavMenu.List className="NavBar-list">
                 <NavMenu.Item>
-                    <NavMenu.Trigger className="NavBar-trigger">Home</NavMenu.Trigger>
+                    <NavMenu.Link className='link' href="/aboutme">Status</NavMenu.Link>
                 </NavMenu.Item>
                 <NavMenu.Item>
-                    <NavMenu.Trigger className="NavBar-trigger">About Me</NavMenu.Trigger>
-                </NavMenu.Item>
-                <NavMenu.Item>
-                    <NavMenu.Trigger className="NavBar-trigger">Projects</NavMenu.Trigger>
+                    <NavMenu.Link className='link' href="/projects">Status</NavMenu.Link>
                 </NavMenu.Item>
             </NavMenu.List>
         </NavMenu.Root>
