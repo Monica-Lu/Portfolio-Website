212b090eca0a65efc330afdf006e60e8c42b000c
monicalu <monicalu1128@hotmail.com>
2025-07-07 19:55:11 -0400

    change base

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    base: '/',
+    base: '/Monica-Lu.github.io/',
     build: {outDir: 'build'},
     plugins:[
         react(),
